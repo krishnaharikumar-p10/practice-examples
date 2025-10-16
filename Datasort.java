a29b176215ebffb238a287ef03a0f6a85a40a400
krishnaharikumar-p10 <krishna.harikumar@people10.com>
2025-10-17 00:14:49 +0530

    Sorting objects using Comparable

--- Datasort.java
+++ Datasort.java
@@ -1,60 +1,52 @@
-package modifiers;
 
 import java.util.*;
 
-class Data {
-    String name;
-    int age;
 
-    Data(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-  
+class Data implements  Comparable<Data>{
+	String name;
+	int age;
+	
+	Data(String name , int age){
+		this.name=name;
+		this.age=age;
+	}
 
-    @Override
-    public String toString() {
-        return "name: " + name + " age: " + age;
-    }
+	@Override
+	public String toString() {
+		return "name :" + name + " age :" + age;
+	}
+
+	@Override
+	public int compareTo(Data that) {
+		return this.name.compareTo(that.name);
+	}
+	
+	
 }
 
 public class Datasort {
     public static void main(String[] args) {
-        List<Data> list = new ArrayList<>();
+    	
+    	List<Data> list = new ArrayList<>();
         list.add(new Data("Mike", 22));
         list.add(new Data("Taylor", 20));
         list.add(new Data("Alex", 25));
-
+        
         System.out.println("Before sorting:");
-        for (Data d : list) {
-            System.out.println(d);
+        for (Data d: list) {
+        	System.out.println(d);
         }
         
-        Comparator<Data> comp1 = new Comparator<Data>() {
-            @Override
-            public int compare(Data d1, Data d2) {
-                return (d1.age % 10) - (d2.age % 10);
-            }
-        };
+        Collections.sort(list);
         
-        Comparator<Data> comp2 = new Comparator<Data>() {
-            @Override
-            public int compare(Data d1, Data d2) {
-                return d1.name.compareTo(d2.name);
-            }
-        };
-
-        Collections.sort(list, comp1);
-
-        System.out.println("\n Sorting by last digit of age:");
-        for (Data d : list) {
-            System.out.println(d);
+        System.out.println("After sorting:");
+        for (Data d: list) {
+        	System.out.println(d);
         }
-        Collections.sort(list, comp2);
-
-        System.out.println("\nSorting by name:");
-        for (Data d : list) {
-            System.out.println(d);
-        }
+        
+        
+       
+        
+        
     }
 }
