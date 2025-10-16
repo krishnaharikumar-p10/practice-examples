f96f46b6a4701d38d4158fb69ceb646e30f1eaf9
krishnaharikumar-p10 <krishna.harikumar@people10.com>
2025-10-16 15:03:30 +0530

    Generic Example

---
+++ GenericExample.java
@@ -0,0 +1,41 @@
+package week2;
+
+class Home<T>{
+	
+	public T data;
+	private String name;
+	
+	Home(T data,String name){
+		
+		this.data=data;
+		this.name=name;
+	}
+	
+
+    public String getName() {
+		return name;
+	}
+    
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return "name :"+name+ " " +"data: "+data; 
+	}
+
+}
+
+public class GenericExample {
+
+	public static void main(String[] args) {
+		
+		Home<Integer> home1= new Home<>(897,"michael");
+		System.out.println(home1);
+		
+		Home<String> home3= new Home<>("D293","taylor");
+		System.out.println(home3);
+		
+	
+	}
+}
